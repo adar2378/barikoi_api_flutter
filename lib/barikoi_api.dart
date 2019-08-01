eda35bff284be2fbb5e5feefd2eeee39e8b67f99
unknown <saifulislamadar@gmail.com>
2019-08-01 17:31:45 +0600

    added reversegeocoding

--- lib/barikoi_api.dart
+++ lib/barikoi_api.dart
@@ -2,7 +2,17 @@ class BarikoiApi {
   final String apiKey;
   final String baseurl = "https://barikoi.xyz/v1/api/search/";
   String reveseGeocoding;
-  BarikoiApi(this.apiKey) {
+  String geoCode;
+  String autoComplete;
+  String nearBy;
+  String distance;
+  BarikoiApi({this.apiKey}) {
     reveseGeocoding = "reverse/geocode/$apiKey/place";
+    geoCode = "geocode/$apiKey/place/";
+    autoComplete = "autocomplete/$apiKey/place";
+    nearBy = "nearby/$apiKey/0.5/10";
+    distance = "distance/$apiKey/";
   }
+
+  
 }
---
+++ lib/models/reverse_geocoding.dart
@@ -0,0 +1,9 @@
+class ReverseGeocodingPlace {
+  int id;
+  double distanceWithinMeters;
+  String address;
+  String area;
+  String city;
+  ReverseGeocodingPlace(
+      {this.id, this.distanceWithinMeters, this.address, this.area, this.city});
+}
